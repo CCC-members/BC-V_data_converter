b03a323d7930470c0e8e39bd8ce24f189d559ca8
Ariosky <ariosky.areces@neuroinformatics-collaboratory.org>
2020-01-10 21:07:58 +0800

    including MEG export

--- functions/selected_datastructure_process.m
+++ functions/selected_datastructure_process.m
@@ -55,27 +55,57 @@ if(isequal(selected_data_format.id,'BrainStorm') && is_checked_datastructure_pro
                             subject_info.name = subject.Name;
                         end
                         
-                        if(isfield(selected_data_format, 'preprocessed_eeg'))
-                            if(~isequal(selected_data_format.preprocessed_eeg.base_path,'none'))
-                                filepath = strrep(selected_data_format.preprocessed_eeg.file_location,'SubID',subject.Name);
-                                base_path =  strrep(selected_data_format.preprocessed_eeg.base_path,'SubID',subject.Name);
-                                eeg_file = fullfile(base_path,filepath);
-                                if(isfile(eeg_file))
-                                    disp ("-->> Genering eeg file");
-                                    [hdr, data] = import_eeg_format(eeg_file,selected_data_format.preprocessed_eeg.format);
-                                    labels = hdr.label;
-                                    labels = strrep(labels,'REF','');
-                                    disp ("-->> Removing Channels  by preprocessed EEG");
-                                    [Cdata,Ke] = remove_channels_and_leadfield_from_layout(labels,Cdata,Ke);
-                                    disp ("-->> Sorting Channels and LeadField by preprocessed EEG");
-                                    [Cdata,Ke] = sort_channels_and_leadfield_by_labels(labels,Cdata,Ke);
-                                    
-                                    subject_info.eeg_dir = fullfile('eeg','eeg.mat');
-                                    subject_info.eeg_info_dir = fullfile('eeg','eeg_info.mat');
-                                    disp ("-->> Saving eeg_info file");
-                                    save(fullfile(output_subject_dir,'eeg','eeg_info.mat'),'hdr');
-                                    disp ("-->> Saving eeg file");
-                                    save(fullfile(output_subject_dir,'eeg','eeg.mat'),'data');
+                        if(isfield(selected_data_format, 'preprocessed_data'))
+                            if(~isequal(selected_data_format.preprocessed_data.base_path,'none'))
+                                filepath = strrep(selected_data_format.preprocessed_data.file_location,'SubID',subject.Name);
+                                base_path =  strrep(selected_data_format.preprocessed_data.base_path,'SubID',subject.Name);
+                                data_file = fullfile(base_path,filepath);
+                                if(isfile(data_file))
+                                    if(isequal(selected_data_format.modality,'EEG'))
+                                        disp ("-->> Genering eeg file");
+                                        [hdr, data] = import_eeg_format(data_file,selected_data_format.preprocessed_data.format);
+                                        labels = hdr.label;
+                                        labels = strrep(labels,'REF','');
+                                        disp ("-->> Removing Channels  by preprocessed EEG");
+                                        [Cdata,Ke] = remove_channels_and_leadfield_from_layout(labels,Cdata,Ke);
+                                        disp ("-->> Sorting Channels and LeadField by preprocessed EEG");
+                                        [Cdata,Ke] = sort_channels_and_leadfield_by_labels(labels,Cdata,Ke);
+                                        
+                                        subject_info.eeg_dir = fullfile('eeg','eeg.mat');
+                                        subject_info.eeg_info_dir = fullfile('eeg','eeg_info.mat');
+                                        disp ("-->> Saving eeg_info file");
+                                        save(fullfile(output_subject_dir,'eeg','eeg_info.mat'),'hdr');
+                                        disp ("-->> Saving eeg file");
+                                        save(fullfile(output_subject_dir,'eeg','eeg.mat'),'data');
+                                    else
+                                        disp ("-->> Genering meg file");
+                                        meg = load(data_file);
+                                        hdr = meg.data.hdr;
+                                        fsample = meg.data.fsample;
+                                        trialinfo = meg.data.trialinfo;
+                                        grad = meg.data.grad;
+                                        time = meg.data.time;
+                                        label = meg.data.label;
+                                        cfg = meg.data.cfg;
+                                        %                 labels = strrep(labels,'REF','');
+                                        disp ("-->> Removing Channels  by preprocessed MEG");
+                                        [Cdata,Ke] = remove_channels_and_leadfield_from_layout(label,Cdata,Ke);
+                                        disp ("-->> Sorting Channels and LeadField by preprocessed MEG");
+                                        [Cdata,Ke] = sort_channels_and_leadfield_by_labels(label,Cdata,Ke);
+                                        
+                                        data = [meg.data.trial];
+                                        trials = meg.data.trial;
+                                        
+                                        subject_info.meg_dir = fullfile('meg','meg.mat');
+                                        subject_info.meg_info_dir = fullfile('meg','meg_info.mat');
+                                        subject_info.trials_dir = fullfile('meg','trials.mat');
+                                        disp ("-->> Saving meg_info file");
+                                        save(fullfile(output_subject_dir,'meg','meg_info.mat'),'hdr','fsample','trialinfo','grad','time','label','cfg');
+                                        disp ("-->> Saving meg file");
+                                        save(fullfile(output_subject_dir,'meg','meg.mat'),'data');
+                                        disp ("-->> Saving meg trials file");
+                                        save(fullfile(output_subject_dir,'meg','trials.mat'),'trials')
+                                    end
                                 end
                             end
                         end
@@ -98,7 +128,7 @@ if(isequal(selected_data_format.id,'BrainStorm') && is_checked_datastructure_pro
             disp('No one protocol in this foldes:');
             disp('C:\Users\Ariosky\.brainstorm\local_db');
         end
-    end    
+    end
 elseif(isequal(selected_data_format.id,'ipd'))
     import_preprossed_data(selected_data_format);
 end
