bf5d8d8ca43267c6571066c4a1db006a3d548f0a
Ariosky <ariosky.areces@neuroinformatics-collaboratory.org>
2020-09-04 21:37:31 +0800

    Including PLG data reader plugin

--- Main.m
+++ Main.m
@@ -26,6 +26,7 @@ addpath(fullfile('app'));
 addpath(fullfile('functions'));
 addpath(fullfile('tools'));
 addpath(fullfile('templates'));
+addpath(genpath('plugins'));
 % addpath(strcat('bst_lf_ppl',filesep,'guide'));
 %app_properties = jsondecode(fileread(strcat('properties',filesep,'app_properties.json')));
 app_properties = jsondecode(fileread(strcat('app',filesep,'app_properties.json')));
